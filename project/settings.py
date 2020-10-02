3c188e1bed60d5dc4dd4694a51197b9daff703e6
pietelite <pietelite@gmail.com>
2020-10-02 15:21:44 -0400

    trying to directory to list of places

--- project/settings.py
+++ project/settings.py
@@ -126,10 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = '/home/site/wwwroot/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    '/home/site/wwwroot/pieter/static/'
 ]
 
 STATIC_URL = '/static/'
