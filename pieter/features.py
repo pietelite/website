970ef29b2300650b7df2444d77375e767e2f1fc2
pietelite <pietelite@gmail.com>
2020-10-03 16:15:10 -0400

    minimalistic logos

--- pieter/features.py
+++ pieter/features.py
@@ -5,19 +5,35 @@ static_folder = settings.STATIC_URL
 features  = [
     {
         'name': 'StackOverflow',
-        'image': static_folder + 'pieter/stackoverflow.png',
-        'width': 90,
+        'image': static_folder + 'pieter/stackoverflow-white.png',
+        'width': 70,
         'height': 90,
         'destination': 'https://stackoverflow.com/users/11912161/pieter-svenson',
-        'aura': 'orange'
+        'aura': '#FF9900'
     },
     {
         'name': 'LinkedIn',
-        'image': static_folder + 'pieter/linkedin.png',
-        'width': 100,
-        'height': 100,
+        'image': static_folder + 'pieter/linkedin-white.png',
+        'width': 80,
+        'height': 80,
         'destination': 'https://www.linkedin.com/in/pietersvenson',
-        'aura': 'blue'
+        'aura': '#0072b1'
+    },
+    {
+        'name': 'PlanSite',
+        'image': static_folder + 'pieter/hexagon.png',
+        'width': 90,
+        'height': 105,
+        'destination': 'https://www.plansite3d.com',
+        'aura': 'yellow'
+    },
+    {
+        'name': 'Email',
+        'image': static_folder + 'pieter/mail-white.png',
+        'width': 110,
+        'height': 110,
+        'destination': 'mailto:pieter2@illinois.edu',
+        'aura': 'green'
     },
     {
         'name': 'GitHub',
@@ -26,22 +42,6 @@ features  = [
         'height': 100,
         'destination': 'https://www.github.com/pietelite',
         'aura': 'red'
-    },
-    {
-        'name': 'Email',
-        'image': static_folder + 'pieter/mail.png',
-        'width': 90,
-        'height': 90,
-        'destination': 'mailto:pieter2@illinois.edu',
-        'aura': 'green'
-    },
-    {
-        'name': 'PlanSite',
-        'image': static_folder + 'pieter/plansite.png',
-        'width': 100,
-        'height': 115,
-        'destination': 'https://www.plansite3d.com',
-        'aura': 'yellow'
     }
 ]
 
