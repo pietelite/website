53d7eb71a50a30c59e5e928eb6f0e9907fa4dd9a
pietelite <pietelite@gmail.com>
2020-10-03 01:08:12 -0400

    added auras

--- pieter/features.py
+++ pieter/features.py
@@ -1,22 +1,49 @@
 from django.conf import settings
 
-class Feature:
-    test = "testing"
-
-    def __init__(self, image, destination, static=True):
-        self.image = image
-        self.destination = destination
-        self.static = static
-
-    def zip(self):
-        return {'image': self.image, 'destination': self.destination}
-
-
 static_folder = settings.STATIC_URL
 
-all_features = {
-    'github': Feature(static_folder + 'pieter/github.png', 'https://www.github.com/pietelite').zip(),
-    'linkedin': Feature(static_folder + 'pieter/linkedin.png', 'https://www.linkedin.com/in/pietersvenson').zip(),
-    'stackoverflow': Feature(static_folder + 'pieter/stackoverflow.png', 'https://stackoverflow.com/users/11912161/pieter-svenson').zip(),
-    'email': Feature(static_folder + 'pieter/mail.png', 'mailto:pieter2@illinois.edu').zip()
-}
+features  = [
+    {
+        'name': 'StackOverflow',
+        'image': static_folder + 'pieter/stackoverflow.png',
+        'width': 90,
+        'height': 90,
+        'destination': 'https://stackoverflow.com/users/11912161/pieter-svenson',
+        'aura': 'orange'
+    },
+    {
+        'name': 'LinkedIn',
+        'image': static_folder + 'pieter/linkedin.png',
+        'width': 100,
+        'height': 100,
+        'destination': 'https://www.linkedin.com/in/pietersvenson',
+        'aura': 'blue'
+    },
+    {
+        'name': 'GitHub',
+        'image': static_folder + 'pieter/github.png',
+        'width': 100,
+        'height': 100,
+        'destination': 'https://www.github.com/pietelite',
+        'aura': 'red'
+    },
+    {
+        'name': 'Email',
+        'image': static_folder + 'pieter/mail.png',
+        'width': 90,
+        'height': 90,
+        'destination': 'mailto:pieter2@illinois.edu',
+        'aura': 'green'
+    },
+    {
+        'name': 'PlanSite',
+        'image': static_folder + 'pieter/plansite.png',
+        'width': 100,
+        'height': 115,
+        'destination': 'https://www.plansite3d.com',
+        'aura': 'yellow'
+    }
+]
+
+for feature in features:
+    feature['aura_size'] = max(feature['width'], feature['height'])/2
