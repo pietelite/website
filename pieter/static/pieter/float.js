373e452391b0ab0736d43ffdf5b3cee59849daa3
pietelite <pietelite@gmail.com>
2020-10-07 22:36:25 -0400

    improved animations and device specific logic

--- pieter/static/pieter/float.js
+++ pieter/static/pieter/float.js
@@ -3,10 +3,10 @@ import animate, {stop, delay} from "/static/pieter/animateplus.js"
 const STAR_SIZE_RANGE = [0.5, 8];
 
 /* (per thousand pixels squared (1 mil pixels)) */
-const STAR_DENSITY = 700;
+const STAR_DENSITY = 300;
 
 /* (pixels per second) */
-const STAR_SPEED_RANGE = [5, 30];
+const STAR_SPEED_RANGE = [5, 32];
 
 /* [0:1]
  * A higher size variation means more
@@ -18,11 +18,15 @@ const STAR_ZINDEX_VARIATION = 0.1;
  * A higher speed variation means more variation of speed
  * in stars of the same size.
  */
-const STAR_SPEED_VARIATION = 0.3;
+const STAR_SPEED_VARIATION = 0.5;
 
 const ORBIT_RADIUS = 200;
 
 const ORBIT_SPEED = 1;
+const ORBIT_SPEED_INIT_SPIN = 25
+
+/* duration of time to show elements sequentially, in milliseconds */
+const SHOW_DURATION = 1000; /* 1000 */
 
 /* possible star colors */
 const STAR_PALETTE = [
@@ -34,6 +38,8 @@ const STAR_PALETTE = [
     "FADFAA"
 ];
 
+const has_cursor = window.matchMedia("pointer") != "none";
+
 const random = (min, max) => Math.random() * (max - min) + min;
 
 const vary = (value, variation) => value * random(1-variation/2, 1+variation/2);
@@ -42,48 +48,71 @@ const ratio = (value, min, max) => (value - min) / (max - min);
 
 const float = (config) => {
 
-    const run_stars = () => {
+    /*
+     * ======|
+     * STARS |
+     * ======|
+     */
 
-        const generate_stars = (count, left_pos, top_pos, visible) =>
-            Array.from({length: count}, (_, index) => {
-                var circle = document.createElement("span");
-                const size = random(STAR_SIZE_RANGE[1], STAR_SIZE_RANGE[0]);
-                Object.assign(circle.style, {
-                    width: `${size}px`,
-                    height: `${size}px`,
-                    left: `${left_pos()}vw`,
-                    top: `${top_pos()}vh`,
-                    background: `#${STAR_PALETTE[Math.floor(random(0, STAR_PALETTE.length))]}`,
-                    visibility: (visible ? 'visible' : 'hidden'),
-                    zIndex: Math.floor(vary(ratio(size, STAR_SIZE_RANGE[0], STAR_SIZE_RANGE[1]) * star_count, STAR_ZINDEX_VARIATION))
-                    });
-                Object.assign(circle, {
-                    class: 'star',
-                    size: size,
-                    speed: Math.floor(random(STAR_SPEED_RANGE[0], STAR_SPEED_RANGE[1])),
-                    show: () => circle.style.visibility = 'visible',
-                    hide: () => circle.style.visibility = 'hidden'
+    const generate_stars = (count, left_pos, top_pos, visible) =>
+        Array.from({length: count}, (_, index) => {
+            var circle = document.createElement("span");
+            const size = random(STAR_SIZE_RANGE[1], STAR_SIZE_RANGE[0]);
+            Object.assign(circle.style, {
+                position: 'absolute',
+                width: `${size}px`,
+                height: `${size}px`,
+                left: `${left_pos()}vw`,
+                top: `${top_pos()}vh`,
+                background: `#${STAR_PALETTE[Math.floor(random(0, STAR_PALETTE.length))]}`,
+                visibility: (visible ? 'visible' : 'hidden'),
+                zIndex: Math.floor(vary(ratio(size, STAR_SIZE_RANGE[0], STAR_SIZE_RANGE[1]) * star_count, STAR_ZINDEX_VARIATION)),
+                opacity: 0
                 });
-                return circle;
+            Object.assign(circle, {
+                class: 'star',
+                size: size,
+                speed: Math.floor(random(STAR_SPEED_RANGE[0], STAR_SPEED_RANGE[1])),
+                show: () => circle.style.visibility = 'visible',
+                hide: () => circle.style.visibility = 'hidden'
             });
+            return circle;
+        });
 
-        const star_count = Math.round(STAR_DENSITY * window.innerHeight * window.innerWidth / 1000000);
+    const star_count = Math.round(STAR_DENSITY * window.innerHeight * window.innerWidth / 1000000);
 
-        const orig_stars = generate_stars(star_count, () => random(0, 100), () => random(0, 100), true);
-        const perm_stars = generate_stars(star_count, () => 100, () => random(0, 100), false);
+    const orig_stars = generate_stars(star_count, () => random(0, 100), () => random(0, 100), true);
+    const perm_stars = generate_stars(star_count, () => 100, () => random(0, 100), false);
 
-        document.getElementById("universe").append(...orig_stars);
-        document.getElementById("universe").append(...perm_stars);
+    document.getElementById("universe").append(...orig_stars);
+    document.getElementById("universe").append(...perm_stars);
 
-        const duration_fun = (size) => {
-            const star_size_ratio = (size - STAR_SIZE_RANGE[0]) / (STAR_SIZE_RANGE[1] - STAR_SIZE_RANGE[0]);
-            const star_speed = (STAR_SPEED_RANGE[1] - STAR_SPEED_RANGE[0]) * star_size_ratio + STAR_SPEED_RANGE[0];
-            return window.innerWidth / star_speed;
-        }
+    const duration_fun = (size) => {
+        const star_size_ratio = (size - STAR_SIZE_RANGE[0]) / (STAR_SIZE_RANGE[1] - STAR_SIZE_RANGE[0]);
+        const star_speed = (STAR_SPEED_RANGE[1] - STAR_SPEED_RANGE[0]) * star_size_ratio + STAR_SPEED_RANGE[0];
+        return window.innerWidth / star_speed;
+    }
 
-        const duration_fun_var = (size) => vary(duration_fun(size), STAR_SPEED_VARIATION)
+    const duration_fun_var = (size) => vary(duration_fun(size), STAR_SPEED_VARIATION)
 
-        const avg_duration = window.innerWidth * 2 / (STAR_SPEED_RANGE[1] + STAR_SPEED_RANGE[0])
+    const avg_duration = window.innerWidth * 2 / (STAR_SPEED_RANGE[1] + STAR_SPEED_RANGE[0])
+
+    const show_stars = () => {
+        animate({
+            elements: orig_stars,
+            easing: "linear",
+            duration: SHOW_DURATION,
+            opacity: [0, 1]
+        });
+        animate({
+            elements: perm_stars,
+            easing: "linear",
+            duration: SHOW_DURATION,
+            opacity: [0, 1]
+        });
+    };
+
+    const float_stars = () => {
 
         animate({
             elements: orig_stars,
@@ -92,7 +121,7 @@ const float = (config) => {
             transform: index => [
                 `translate(0vw, -${Math.ceil(orig_stars[index].size/2)}px)`,
                 `translate(-${Math.ceil(100 + orig_stars[index].size/(window.innerWidth)*100)}vw, -${Math.ceil(orig_stars[index].size/2)}px);`]
-        })
+        });
 
         for (var i = 0; i < perm_stars.length; i++) {
             const star = perm_stars[i];
@@ -113,14 +142,83 @@ const float = (config) => {
     }
 
     /*
-     * =========|
-     * FEATURES |
-     * =========|
+     * =======|
+     * SYSTEM |
+     * =======|
      */
 
-     const feature_containers = document.getElementsByClassName("feature-container");
-     const feature_imgs = document.getElementsByClassName("feature-img");
-     const feature_count = feature_containers.length
+    const feature_containers = document.getElementsByClassName("feature-container");
+    const feature_imgs = document.getElementsByClassName("feature-img");
+    const feature_auras = document.getElementsByClassName("feature-aura");
+    const feature_texts = document.getElementsByClassName("feature-text");
+    const feature_text_headers = document.getElementsByClassName("feature-header");
+    const feature_text_descriptions = document.getElementsByClassName("feature-description");
+    const feature_text_description_lines = document.getElementsByClassName("feature-description-line");
+    const feature_links = document.getElementsByClassName("feature-link");
+    const feature_count = feature_containers.length;
+
+    if (!has_cursor) {
+        for (let i = 0; i < feature_links.length; i++) {
+            // remove the hyperlink on the images
+            var wrap = document.createElement("span");
+            wrap.innerHTML = feature_links[i].innerHTML;
+            wrap.className = feature_links[i].className;
+            feature_links[i].parentNode.replaceChild(wrap, feature_links[i])
+
+            // make the header look like a button
+            Object.assign(feature_text_headers[i].style, {
+                textAlign: "center",
+                background: `${feature_auras[i].style.background}`,
+                color: "#e0e0e0",
+                margin: "20px 40px 20px 40px",
+                padding: "10px"
+            });
+
+        }
+        for (let i = 0; i < feature_text_description_lines.length; i++) {
+            Object.assign(feature_text_description_lines[i].style, {
+                textAlign: "center"
+            });
+        }
+    }
+
+    const show_nucleus = () => animate({
+        elements: "#pieter-head",
+        easing: "linear",
+        duration: SHOW_DURATION,
+        opacity: [0, 1]
+    });
+
+    const init_spin_easing_duration = {
+        easing: "in-out-quintic",
+        duration: 60 * 1000 / ORBIT_SPEED_INIT_SPIN
+    };
+
+    const show_features = () => {
+        animate({
+            elements: ".feature-img",
+            ...init_spin_easing_duration,
+            opacity: [0, 1]
+        });
+        animate({
+            elements: ".feature-aura",
+            ...init_spin_easing_duration,
+            opacity: [0, 1]
+        });
+    }
+
+    const get_features_by_id = () => {
+        let map = {};
+        for (let i = 0; i < feature_containers.length; i++) {
+            let feature_id = feature_containers[i].feature_id;
+            map[feature_id] = {};
+            map[feature_id]['container'] = feature_containers[i];
+            map[feature_id]['text'] = feature_texts[i];
+        }
+        return map;
+    }
+
+    const features_by_id = get_features_by_id();
 
     /* ratio of orbit that the features have gone through */
     var orbit_progress = 0;
@@ -140,10 +238,27 @@ const float = (config) => {
 
     const spin_start = () => animate({
         elements: ".feature-container",
-        duration: 1000,
-        easing: "in-out-cubic",
+        ...init_spin_easing_duration,
         transform: index => transform_feature(index, 1, 1, 0, 1),
     });
+
+    let shift_system_x_offset = 0;
+    let shift_system_y_offset = 0;
+
+    if (window.innerWidth > window.innerHeight) {
+        shift_system_x_offset = Math.floor((window.innerWidth - ORBIT_RADIUS*2) / 3)
+    } else {
+        shift_system_y_offset = Math.floor((window.innerHeight - ORBIT_RADIUS*2) / 3)
+    }
+
+    const shift_system = () => {
+        animate({
+            elements: "#pieter-system",
+            easing: "in-out-quartic",
+            duration: 60 * 1000 / ORBIT_SPEED_INIT_SPIN,
+            transform: ["translate(0px, 0px)", `translate(-${shift_system_x_offset}px, -${shift_system_y_offset}px)`]
+        });
+    }
 
     const orbit = () => animate({
         elements: ".feature-container",
@@ -160,38 +275,98 @@ const float = (config) => {
         }
     });
 
-    const init_listeners = () => {
-        for (var i = 0; i < feature_imgs.length; i++) {
-            feature_imgs[i].feature_index = i;
-            feature_imgs[i].addEventListener("mouseover", ({target}) => {
-                stop(".feature-container");
-                animate({
-                    elements: target,
-                    duration: 1500,
-                    easing: "out-elastic 1.3 0.3",
-                    transform: ["scale(1)", "scale(1.6)"]
-                });
+    /*
+     * =====|
+     * TEXT |
+     * =====|
+     */
 
-            });
-            feature_imgs[i].addEventListener("mouseout", ({target}) => {
-                stop(".feature-container");
-                stop(".feature-img");
-                orbit_progress_reset = orbit_progress;
-                animate({
-                    elements: target,
-                    easing: "out-cubic",
-                    duration: 300,
-                    transform: ["scale(1.6)", "scale(1)"]
-                })
-                .then(options => orbit());
-            });
+    let top_offset = 0;
+    let left_offset = 0;
+    if (window.innerWidth > window.innerHeight) {
+        top_offset = window.innerHeight * 0.40;
+        left_offset = window.innerWidth/2 + (window.innerWidth/2 - ORBIT_RADIUS + shift_system_x_offset - 400) / 2;
+    } else {
+        top_offset = Math.max(window.innerHeight * 2/3, window.innerHeight / 2 + ORBIT_RADIUS - shift_system_y_offset + 100);
+        left_offset = (window.innerWidth - 400) / 2;
+    }
+
+    for (let i = 0; i < feature_texts.length; i++) {
+        Object.assign(feature_texts[i].style, {top: `${top_offset}px`, left: `${left_offset}px`});
+    }
+
+    const show_text = (element) => animate({
+        elements: element,
+        duration: 300,
+        easing: "linear",
+        opacity: [0, 0.8]
+    });
+
+
+    const hide_text = (element) => animate({
+        elements: element,
+        duration: 300,
+        easing: "linear",
+        opacity: [0.8, 0]
+    });
+
+
+    /*
+     * ==========|
+     * LISTENERS |
+     * ==========|
+     */
+
+    const init_listeners = () => {
+        for (let i = 0; i < feature_imgs.length; i++) {
+            if (has_cursor) {
+                feature_imgs[i].addEventListener("mouseover", ({target}) => {
+                    stop(".feature-container");
+                    animate({
+                        elements: feature_imgs[i],
+                        duration: 1500,
+                        easing: "out-elastic 1.3 0.3",
+                        transform: ["scale(1)", "scale(1.6)"]
+                    });
+                    show_text(feature_texts[i]);
+                });
+                feature_imgs[i].addEventListener("mouseout", ({target}) => {
+                    stop(".feature-container");
+                    stop(".feature-img");
+                    orbit_progress_reset = orbit_progress;
+                    animate({
+                        elements: target,
+                        easing: "out-cubic",
+                        duration: 300,
+                        transform: ["scale(1.6)", "scale(1)"]
+                    });
+                    hide_text(feature_texts[i]);
+                    orbit();
+                });
+            } else {
+                var disabled_feature_click = false;
+                feature_imgs[i].addEventListener("click", ({target}) => {
+                    if (!disabled_feature_click) {
+                        show_text(feature_texts[i]).then(() => {
+                            disabled_feature_click = true;
+                            delay(2000).then(() => {
+                                disabled_feature_click = false;
+                                hide_text(feature_texts[i])
+                            });
+                        });
+                    }
+                });
+            }
         }
     }
 
-    run_stars();
-    spin_start().then(() => {init_listeners(); orbit();});
+    delay(500).then(() => show_nucleus().then(() => {
+        shift_system();
+        show_features();
+        spin_start().then(() => {init_listeners(); orbit(); show_stars(); float_stars();});
+    }));
 
 
 }
 
-float();
+document.addEventListener("DOMContentLoaded", () => float());
--- pieter/views.py
+++ pieter/views.py
@@ -1,8 +1,12 @@
 from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
-from .features import features as all_features
+from .features import feature_pool
 
 def home(request):
-    context = {'features': all_features}
+    # The features we want to show in order
+    # (data in feature_pool in features.py)
+    features = ['stackoverflow', 'email', 'linkedin', 'instagram', 'plansite', 'github']
+
+    context = {'features': [feature_pool[feature_name] for feature_name in features]}
     return HttpResponse(render(request, 'pieter/home.html', context))
